b21fe38de3afbc4289288d8501572421695b03b6
AcevedoDario <46717303+AcevedoDario@users.noreply.github.com>
2022-05-23 10:13:10 -0300

    Deleted duplicated codelines

--- middleware/handleErrors.js
+++ middleware/handleErrors.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module.exports = (error, request, response, next) => {
     console.error(error)
 
@@ -7,14 +6,4 @@ module.exports = (error, request, response, next) => {
     } else {
         response.status(500).end()
     }
-=======
-module.exports = (error, request, response, next) => {
-    console.error(error)
-
-    if (error.name === 'CastError'){
-        response.status(400).send({ error: 'id used is malformed' })
-    } else {
-        response.status(500).end()
-    }
->>>>>>> 6a857f777ab538a08b457819d6e8ccf19bd0570f
 }
--- middleware/notFound.js
+++ middleware/notFound.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 module.exports = (request, response, next) => {
     response.status(404).end()
-=======
-module.exports = (request, response, next) => {
-    response.status(404).end()
->>>>>>> 6a857f777ab538a08b457819d6e8ccf19bd0570f
 }
