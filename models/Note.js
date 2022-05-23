4b0840dda39b4410cac8b2aee008b9644400efdc
AcevedoDario <46717303+AcevedoDario@users.noreply.github.com>
2022-05-23 10:19:39 -0300

    Edited Note schema, now has a user attribute

--- models/Note.js
+++ models/Note.js
@@ -1,14 +1,17 @@
-<<<<<<< HEAD
 const { model, Schema} = require('mongoose')
 
-//crear esquema
+//creating a new schema for the notes
 const noteSchema = new Schema({
     content: String,
     date: Date,
-    important: Boolean
+    important: Boolean,
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
 })
 
-//editar el metodo toJSON para que no muestre el campo _id y __v
+//editing the schema deleting the _id and the __v properties
 noteSchema.set('toJSON', {
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id
@@ -17,30 +20,7 @@ noteSchema.set('toJSON', {
     }
 })
 
-//crear modelo
+//creating a new model for the notes
 const Note = model('Note', noteSchema)
 
-=======
-const { model, Schema} = require('mongoose')
-
-//crear esquema
-const noteSchema = new Schema({
-    content: String,
-    date: Date,
-    important: Boolean
-})
-
-//editar el metodo toJSON para que no muestre el campo _id y __v
-noteSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-})
-
-//crear modelo
-const Note = model('Note', noteSchema)
-
->>>>>>> 6a857f777ab538a08b457819d6e8ccf19bd0570f
 module.exports = Note
