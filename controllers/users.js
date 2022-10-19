01d43cd63b9ec1cb8b5a7331fb57807f0d012667
AcevedoDario <dario_acevedo_1990@hotmail.com>
2022-10-19 08:44:52 -0300

    Created path to get all users

--- controllers/users.js
+++ controllers/users.js
@@ -1,28 +1,31 @@
-const usersRouter = require('express').Router()
-const User = require('../models/User')
-const bcrypt = require('bcrypt')
+const usersRouter = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-usersRouter.post('/', async(request, response) => {
-    
-    try{
-        const {body} = request
-        const {username, name, password} = body
-    
-        const saltRounds = 10
-        const passwordHash = await bcrypt.hash(password, saltRounds)
-    
-        const user = new User({
-            username,
-            name,
-            passwordHash
-        })
-    
-        const savedUser = await user.save()
-        response.status(201).json(savedUser)
+usersRouter.get("/", async (request, response) => {
+  const users = await User.find({});
+  response.json(users);
+});
 
-    } catch(error){
-        response.status(409).json(error)
-    }    
-})
+usersRouter.post("/", async (request, response) => {
+  try {
+    const { body } = request;
+    const { username, name, password } = body;
 
-module.exports = usersRouter
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
+
+    const user = new User({
+      username,
+      name,
+      passwordHash,
+    });
+
+    const savedUser = await user.save();
+    response.status(201).json(savedUser);
+  } catch (error) {
+    response.status(409).json(error);
+  }
+});
+
+module.exports = usersRouter;
