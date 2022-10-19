8b89b6bcad050aefae57d9a06e77b458239f53ce
AcevedoDario <dario_acevedo_1990@hotmail.com>
2022-10-19 11:26:45 -0300

    deleted an useless path and edited the get all notes path to show all notes correctly

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,78 +1,83 @@
-const notesRouter = require('express').Router()
-const Note = require('../models/Note')
+const notesRouter = require("express").Router();
+const User = require("../models/User");
+const Note = require("../models/Note");
 
-notesRouter.get('/',(request, response) => {
-    response.send('<h1>Bienvenido a mi API</h1>')
-  })
-  
-  //Get all notes
-  notesRouter.get('/api/notes', async(request, response) => {
-    const notes = await Note.find({})
-    response.json(notes)
-  })
-  
-  //Get a specific note
-  notesRouter.get('/:id', (request, response, next) => {
-    const { id } = request.params
-    
-    Note.findById(id).then(note => {
+//Get all notes
+notesRouter.get("/", async (request, response) => {
+  const notes = await Note.find({});
+  response.json(notes);
+});
+
+//Get a specific note
+notesRouter.get("/:id", (request, response, next) => {
+  const { id } = request.params;
+
+  Note.findById(id)
+    .then((note) => {
       if (note) {
-        return response.json(note)
+        return response.json(note);
       } else {
-        response.status(404).end()
+        response.status(404).end();
       }
-    }).catch(err => {
-      next(err)
     })
-  })
-  
-  //Update a specific note
-  notesRouter.put('/:id', (request, response, next) => {
-    const { id } = request.params
-    const note = request.body
-  
-    const newNoteInfo = {
-      content: note.content,
-      important: note.important
-    }
-  
-    Note.findByIdAndUpdate(id, newNoteInfo, { new : true})
-      .then(result =>{
-        response.json(result)
-      })
-  })
-  
-  //Delete a note
-  notesRouter.delete('/:id', (request, response, next) => {
-    const { id } = request.params
-  
-    Note.findByIdAndDelete(id)
+    .catch((err) => {
+      next(err);
+    });
+});
+
+//Update a specific note
+notesRouter.put("/:id", (request, response, next) => {
+  const { id } = request.params;
+  const note = request.body;
+
+  const newNoteInfo = {
+    content: note.content,
+    important: note.important,
+  };
+
+  Note.findByIdAndUpdate(id, newNoteInfo, { new: true }).then((result) => {
+    response.json(result);
+  });
+});
+
+//Delete a note
+notesRouter.delete("/:id", (request, response, next) => {
+  const { id } = request.params;
+
+  Note.findByIdAndDelete(id)
     .then(() => response.status(204).end())
-    .catch(next)
-  })
+    .catch(next);
+});
 
 //Post a new note
-notesRouter.post('/', async(request, response) => {
-    const note = request.body
-    //validar que el contenido no esté vacio
-    if ( !note.content ) {
-      return response.status(400).json({
-        error: 'required "content" field is missing'
-      })
-    }
-  
-    const newNote = new Note({
-      content: note.content,
-      date: new Date(),
-      important: note.important || false
-    })
-  
-    try {
-      const savedNote = await newNote.save()
-      response.json(savedNote)
-    } catch (error) {
-      next(error)
-    }
-  })
+notesRouter.post("/", async (request, response) => {
+  const { content, important = false, userId } = request.body;
 
-module.exports = notesRouter
+  const user = await User.findById(userId);
+  //Check content is not empty
+  if (!content) {
+    return response.status(400).json({
+      error: 'required "content" field is missing',
+    });
+  }
+
+  const newNote = new Note({
+    content,
+    date: new Date(),
+    important,
+    user: user._id,
+  });
+
+  try {
+    const savedNote = await newNote.save();
+
+    user.notes = user.notes.concat(savedNote._id);
+    await user.save();
+
+    response.json(savedNote);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = notesRouter;
