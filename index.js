71f10fae8acb86ab5698a3c27d4184d9be2be06d
AcevedoDario <dario_acevedo_1990@hotmail.com>
2022-10-20 13:49:47 -0300

    Added login router path to index file

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-require('dotenv').config()
-require('./mongo')
+require("dotenv").config();
+require("./mongo");
 
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const notFound = require('./middleware/notFound.js')
-const handleErrors = require('./middleware/handleErrors.js')
-const usersRouter = require('./controllers/users')
-const notesRouter = require('./controllers/notes')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const notFound = require("./middleware/notFound.js");
+const handleErrors = require("./middleware/handleErrors.js");
+const usersRouter = require("./controllers/users");
+const notesRouter = require("./controllers/notes");
+const loginRouter = require("./controllers/login");
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use('/api/notes', notesRouter)
-app.use('/api/users', usersRouter)
-app.use(notFound)
-app.use(handleErrors)
+app.use("/api/notes", notesRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
+app.use(notFound);
+app.use(handleErrors);
 
 //PORT config
-const PORT = process.env.PORT
-const server = app.listen(PORT)
-console.log(`Server is running on port: ${PORT}.`)
+const PORT = process.env.PORT;
+const server = app.listen(PORT);
+console.log(`Server is running on port: ${PORT}.`);
 
-module.exports = {app, server}
+module.exports = { app, server };
